6b6759e6f6f9eae9c550157829cefc2f06c525ee
RogerioVieira77 <rogerio.vieira77@hotmail.com>
2025-10-22 18:21:25 -0300

    Backup Inicial

--- backup_db.sh
+++ backup_db.sh
@@ -5,10 +5,13 @@ BACKUP_DIR="/var/www/smartceu/backups"
 DATE=$(date +%Y%m%d_%H%M%S)
 BACKUP_FILE="${BACKUP_DIR}/smartceu_${DATE}.sql.gz"
 
+# Criar diretório se não existir
+mkdir -p ${BACKUP_DIR}
+
 # Remover backups com mais de 7 dias
 find ${BACKUP_DIR} -name "smartceu_*.sql.gz" -mtime +7 -delete
 
-# Fazer backup
-mysqldump -u smartceu_user -p'SmartCEU2025!Secure' smartceu_db | gzip > ${BACKUP_FILE}
+# Fazer backup (sem tablespaces para evitar warning)
+mysqldump -u smartceu_user -p'SmartCEU2025!Secure' --no-tablespaces smartceu_db | gzip > ${BACKUP_FILE}
 
 echo "Backup criado: ${BACKUP_FILE}"
